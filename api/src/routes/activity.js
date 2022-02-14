b86ea22e849a86f732881ed37135301ba9816cba
Elian <eliancarlogm@gmail.com>
2022-02-13 22:25:35 -0430

    refactor(api/routes) control errors and indentation

--- api/src/routes/activity.js
+++ api/src/routes/activity.js
@@ -1,47 +1,43 @@
 const { Router } = require("express");
 const router = Router();
-const { Activity,Country } = require("../db");
+const { Activity, Country } = require("../db");
 
+router.get("/", async (req, res) => {
+  try {
+    const activities = await Activity.findAll({ include: Country });
+    return res.json(activities);
+  } catch (error) {
+    console.log(error);
+  }
+});
 
-router.get('/',async(req,res)=>{
-    try {
-        const activities = await Activity.findAll({include:Country})
-        return res.json(activities)
-    } catch (error) {
-        console.log(error)
-    }
-   
+router.post("/", async (req, res) => {
+  try {
+    const { name, dificultad, duracion, temporada, countries } = req.body;
 
-})
-
-router.post('/', async(req,res)=>{
-    try {
-        const {name,dificultad,duracion,temporada,countries} = req.body;
-
-      console.log(temporada);
-        const createActivity = await Activity.create({
-            name,
-            difficulty: dificultad,
-            season: `${temporada}`,
-            duration: duracion,
-        });
-        countries.forEach(async (country) => {
-            let activityCountry = await Country.findOne({
-                where: {
-                    ID: country
-                }
-            }) 
-            await createActivity.addCountry(activityCountry)
-        });
-        console.log(req.body)
-        res.json({msg:'se creo exitosamente'});
-    } catch (error) {
-        console.log(error);
-    }
-   
-})
-
-
-
+    console.log(temporada);
+    const createActivity = await Activity.create({
+      name,
+      difficulty: dificultad,
+      season: `${temporada}`,
+      duration: duracion,
+    });
+    countries.forEach(async (country) => {
+      let activityCountry = await Country.findOne({
+        where: {
+          ID: country,
+        },
+      });
+      await createActivity.addCountry(activityCountry);
+    });
+    console.log(req.body);
+    res.json({ msg: "se creo exitosamente" });
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(400)
+      .json({ errorMsg: "No se pudo crear la actividad", err });
+  }
+});
 
 module.exports = router;
--- api/src/routes/countries.js
+++ api/src/routes/countries.js
@@ -1,67 +1,64 @@
 const { Router } = require("express");
 const router = Router();
-const axios = require("axios");
 const { Country, Activity } = require("../db");
 const { Op } = require("sequelize");
 
-
-router.get("/",  (req, res) => {
-   const {name} = req.query;
-   //console.log(nameLower);
-   /* axios.get("https://restcountries.com/v3/all")
+router.get("/", (req, res) => {
+  const { name } = req.query;
+  //console.log(nameLower);
+  /* axios.get("https://restcountries.com/v3/all")
    .then((data)=>{
       return res.json(data.data);
    }); */
-   if(name){
-      let nameLower = name.toLowerCase();
-      return Country.findAll({
-         where: {
-            name: {[Op.startsWith]: nameLower},
-         },
-         include: {
-            model: Activity,
-         }
+  if (name) {
+    let nameLower = name.toLowerCase();
+    return Country.findAll({
+      where: {
+        name: { [Op.startsWith]: nameLower },
+      },
+      include: {
+        model: Activity,
+      },
+    })
+      .then((countries) => {
+        if (countries.length == 0) {
+          return res.json("Sin coincidencias");
+        } else {
+          return res.json(countries);
+        }
+        //console.log(countries);
       })
-      .then((countries)=>{
-         if(countries.length == 0){
-            return res.json("Sin coincidencias");
-         } else {
-            return res.json(countries); 
-         }
-         //console.log(countries);
-      });
-   } else {
-      return Country.findAll({
-            include: {
-                model: Activity,
-            }
-        })
-      .then((countries)=>{
-         //console.log(countries);
-          return res.json(countries); 
-      }).catch((rr)=> console.log(rr));
-   }
+      .catch((err) => console.log(err));
+  } else {
+    return Country.findAll({
+      include: {
+        model: Activity,
+      },
+    })
+      .then((countries) => {
+        //console.log(countries);
+        return res.json(countries);
+      })
+      .catch((err) => console.log(err));
+  }
 });
-
-
 
 router.get("/:idPais", (req, res) => {
-   const {idPais} = req.params;
-   let idPaisUpper = idPais.toUpperCase();
-   //console.log(idPaisUpper);
-   return Country.findByPk(idPaisUpper,{
-        include : {
-            model : Activity
-        }
-    })
-   .then((country)=>{
+  const { idPais } = req.params;
+  let idPaisUpper = idPais.toUpperCase();
+  //console.log(idPaisUpper);
+  return Country.findByPk(idPaisUpper, {
+    include: {
+      model: Activity,
+    },
+  })
+    .then((country) => {
       //console.log(country);
       return res.json(country);
-   });
+    })
+    .catch((err) =>
+      res.status(404).json({ errorMsg: "ID de pais no encontrado", err })
+    );
 });
-
-
-
-
 
 module.exports = router;
