d8e03cfafc1747234f9b045b45400b1efe2b92e1
Elian <eliancarlogm@gmail.com>
2022-02-15 20:06:23 -0430

    refactor(client/CountryCard) Change css to Styled Component

--- client/src/components/CountryCard.js
+++ client/src/components/CountryCard.js
@@ -1,13 +1,41 @@
 import React from "react";
-import "./CountryCard.css";
+import styled from "styled-components";
+
+const CountryCardContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: var(--gray-light-color);
+  color: var(--gray-dark-color);
+  cursor: pointer;
+  transition: ease transform 0.5s;
+
+  &:hover {
+    transform: scale(1.2);
+  }
+
+  & h4 {
+    text-align: center;
+    text-transform: capitalize;
+  }
+
+  & p {
+    text-align: center;
+    text-transform: capitalize;
+  }
+`;
+
+const CountryImg = styled.img`
+  width: 100%;
+`;
 
 const CountryCard = ({ name, flagImg, continent }) => {
   return (
-    <div className="countryCard margin-bottom">
+    <CountryCardContainer>
+      <CountryImg src={flagImg} alt={name} />
       <h4>{name}</h4>
-      <img src={flagImg} alt={name} />
       <p>{continent}</p>
-    </div>
+    </CountryCardContainer>
   );
 };
 
