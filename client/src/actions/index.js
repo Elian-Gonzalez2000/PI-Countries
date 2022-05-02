c6221cd25f6d73223a9f907a1f0d278a41069310
Elian <eliancarlogm@gmail.com>
2022-05-01 22:28:13 -0430

    Add connection string constant

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -8,46 +8,49 @@ export const FILTER_CONTINENT = "FILTER_CONTINENT";
 export const FILTER_ACTIVITY = "FILTER_ACTIVITY";
 export const ORDER_BY_POBLATION = "ORDER_BY_POBLATION";
 export const GET_LOAD_STATUS = "GET_LOAD_STATUS";
+export const connectionString = true
+   ? "https://find-countries.herokuapp.com"
+   : "http://localhost:3001";
 
 export function setLoadStatus(payload) {
-  return {
-    type: GET_LOAD_STATUS,
-    payload,
-  };
+   return {
+      type: GET_LOAD_STATUS,
+      payload,
+   };
 }
 
 export function addActivity(payload) {
-  return { type: "ADD_ACTIVITY", payload };
+   return { type: "ADD_ACTIVITY", payload };
 }
 
 export function getCountries() {
-  return (dispatch) => {
-    dispatch(setLoadStatus(true));
-    return fetch(`http://localhost:3001/countries/`)
-      .then((response) => response.json())
-      .then((json) => {
-        dispatch(setLoadStatus(false));
-        return dispatch({ type: GET_COUNTRIES, payload: json });
-      });
-  };
+   return (dispatch) => {
+      dispatch(setLoadStatus(true));
+      return fetch(`${connectionString}/countries/`)
+         .then((response) => response.json())
+         .then((json) => {
+            dispatch(setLoadStatus(false));
+            return dispatch({ type: GET_COUNTRIES, payload: json });
+         });
+   };
 }
 
 export function getCountriesByName(input) {
-  return (dispatch) => {
-    dispatch(setLoadStatus(true));
-    return fetch(`http://localhost:3001/countries?name=${input}`)
-      .then((response) => response.json())
-      .then((json) => {
-        dispatch(setLoadStatus(false));
-        return dispatch({ type: GET_COUNTRIES_BY_NAME, payload: json });
-      });
-  };
+   return (dispatch) => {
+      dispatch(setLoadStatus(true));
+      return fetch(`${connectionString}/countries?name=${input}`)
+         .then((response) => response.json())
+         .then((json) => {
+            dispatch(setLoadStatus(false));
+            return dispatch({ type: GET_COUNTRIES_BY_NAME, payload: json });
+         });
+   };
 }
 
 /* export function getCountryDetails(id) {
   return (dispatch) => {
     dispatch(setLoadStatus(true));
-    return fetch(`http://localhost:3001/countries/${id}`)
+    return fetch(`${connectionString}/countries/${id}`)
       .then((response) => response.json())
       .then((json) => {
         dispatch(setLoadStatus(false));
@@ -57,57 +60,57 @@ export function getCountriesByName(input) {
 }
  */
 export function getActivities() {
-  return (dispatch) => {
-    return fetch(`http://localhost:3001/activity/`)
-      .then((response) => response.json())
-      .then((json) => {
-        return dispatch({ type: GET_ACTIVITIES, payload: json });
-      });
-  };
+   return (dispatch) => {
+      return fetch(`${connectionString}/activity/`)
+         .then((response) => response.json())
+         .then((json) => {
+            return dispatch({ type: GET_ACTIVITIES, payload: json });
+         });
+   };
 }
 
 export function postActivities(payload) {
-  return (dispatch) => {
-    fetch(`http://localhost:3001/activity/`, {
-      method: "POST", // or "PUT"
-      body: JSON.stringify(payload), // data can be `string` or {object}!
-      headers: {
-        "Content-Type": "application/json",
-      },
-    })
-      .then((res) => res.json())
-      .catch((error) => console.error("Error:", error))
-      .then((data) => {
-        console.log("Success:", data);
-        return data;
-      });
-  };
+   return (dispatch) => {
+      fetch(`${connectionString}/activity/`, {
+         method: "POST", // or "PUT"
+         body: JSON.stringify(payload), // data can be `string` or {object}!
+         headers: {
+            "Content-Type": "application/json",
+         },
+      })
+         .then((res) => res.json())
+         .catch((error) => console.error("Error:", error))
+         .then((data) => {
+            console.log("Success:", data);
+            return data;
+         });
+   };
 }
 
 export function orderName(payload) {
-  return {
-    type: ORDER_BY_NAME,
-    payload,
-  };
+   return {
+      type: ORDER_BY_NAME,
+      payload,
+   };
 }
 
 export function orderByPoblation(payload) {
-  return {
-    type: ORDER_BY_POBLATION,
-    payload,
-  };
+   return {
+      type: ORDER_BY_POBLATION,
+      payload,
+   };
 }
 
 export function filterByContinent(payload) {
-  return {
-    type: FILTER_CONTINENT,
-    payload,
-  };
+   return {
+      type: FILTER_CONTINENT,
+      payload,
+   };
 }
 
 export function filterByActivity(payload) {
-  return {
-    type: FILTER_ACTIVITY,
-    payload,
-  };
+   return {
+      type: FILTER_ACTIVITY,
+      payload,
+   };
 }
