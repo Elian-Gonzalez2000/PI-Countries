43c463f4657c554c3309f8f1947f322f144a89bb
Elian Gonzalez <eliancarlogm@gmail.com>
2023-12-28 19:04:04 -0400

    refactor: change connection string

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -8,9 +8,10 @@ export const FILTER_CONTINENT = "FILTER_CONTINENT";
 export const FILTER_ACTIVITY = "FILTER_ACTIVITY";
 export const ORDER_BY_POBLATION = "ORDER_BY_POBLATION";
 export const GET_LOAD_STATUS = "GET_LOAD_STATUS";
-export const connectionString = true
-   ? "https://countries-api-8qqr.onrender.com"
-   : "http://localhost:3001";
+export const connectionString =
+   window.location.host === "elian-gonzalez2000.github.io"
+      ? "https://countries-api-8qqr.onrender.com"
+      : "http://localhost:3001";
 
 export function setLoadStatus(payload) {
    return {
