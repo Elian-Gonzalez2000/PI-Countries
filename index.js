ec6235bd625d57551471ccda8b957503972aaba5
unknown <eliancarlogm@gmail.com>
2025-01-20 00:01:03 +0100

    Fix DB conection database

--- index.js
+++ index.js
@@ -24,27 +24,44 @@ const axios = require("axios");
 
 // Syncing all the models at once.
 conn.sync({ force: false }).then(async () => {
-   const countriesValidation = await Country.findAll();
-   if (countriesValidation.length === 0) {
-      const apiResponse = await axios.get("https://restcountries.com/v3/all");
+  const countriesValidation = await Country.findAll();
+  if (countriesValidation.length === 0) {
+    try {
+      const apiResponse = await axios.get("https://restcountries.com/v3/all", {
+        timeout: 60000,
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+          "User-Agent": "axios/0.25.0",
+        },
+      });
+      if (apiResponse.data.length === 0) {
+        console.log("No se encontraron paises");
+        return;
+      }
       const apiResults = apiResponse.data;
       let api = apiResults.map((country) => {
-         return {
-            ID: country.cca3,
-            name: country.name.common.toLowerCase(),
-            flagImg: country.flags[0],
-            continent: country.continents[0],
-            subregion: country.subregion,
-            capital: country.capital ? country.capital[0] : "No Tiene",
-            area: country.area,
-            population: country.population,
-         };
+        return {
+          ID: country.cca3,
+          name: country.name.common.toLowerCase(),
+          flagImg: country.flags[0],
+          continent: country.continents[0],
+          subregion: country.subregion,
+          capital: country.capital ? country.capital[0] : "No Tiene",
+          area: country.area,
+          population: country.population,
+        };
       });
       //console.log(api);
       await Country.bulkCreate(api);
-   }
+    } catch (error) {
+      console.error("Error message al obtener datos de la API:", error.message);
+      console.error("Error config al obtener datos de la API:", error.config);
+      console.error("Error code al obtener datos de la API:", error.code);
+    }
+  }
 
-   server.listen(process.env.PORT || 3001, () => {
-      console.log("%s listening at 3001"); // eslint-disable-line no-console
-   });
+  server.listen(process.env.PORT || 3001, () => {
+    console.log("%s listening at 3001"); // eslint-disable-line no-console
+  });
 });
--- src/db.js
+++ src/db.js
@@ -2,36 +2,43 @@ require("dotenv").config();
 const { Sequelize } = require("sequelize");
 const fs = require("fs");
 const path = require("path");
-const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_CONNECTION, DATABASE_URL } =
-   process.env;
+const { log } = require("console");
+const {
+  DB_USER,
+  DB_PASSWORD,
+  DB_HOST,
+  DB_PORT,
+  DB_NAME,
+  DB_CONNECTION,
+  DATABASE_URL,
+} = process.env;
 
 const sequelize = new Sequelize(
-   DB_CONNECTION === "local"
-      ? `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/countries`
-      : DATABASE_URL,
-   {
-      logging: false, // set to console.log to see the raw SQL queries
-      native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-      dialectOptions:
-         DB_CONNECTION === "local"
-            ? {}
-            : {
-                 ssl: {
-                    require: true,
-                    rejectUnauthorized: false,
-                 },
-              },
-   }
+  DB_CONNECTION === "local"
+    ? `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`
+    : DATABASE_URL,
+  {
+    dialect: "postgres",
+    protocol: "postgres",
+    logging: false, // set to console.log to see the raw SQL queries
+    native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  }
 );
 
 sequelize
-   .authenticate()
-   .then(() => {
-      console.log("Connection has been established successfully.");
-   })
-   .catch((err) => {
-      console.error("Unable to connect to the database:", err);
-   });
+  .authenticate()
+  .then(() => {
+    console.log("Connection has been established successfully.");
+  })
+  .catch((err) => {
+    console.error("Unable to connect to the database:", err);
+  });
 
 const basename = path.basename(__filename);
 
@@ -39,23 +46,21 @@ const modelDefiners = [];
 
 // Leemos todos los archivos de la carpeta Models, los requerimos y agregamos al arreglo modelDefiners
 fs.readdirSync(path.join(__dirname, "/models"))
-   .filter(
-      (file) =>
-         file.indexOf(".") !== 0 &&
-         file !== basename &&
-         file.slice(-3) === ".js"
-   )
-   .forEach((file) => {
-      modelDefiners.push(require(path.join(__dirname, "/models", file)));
-   });
+  .filter(
+    (file) =>
+      file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
+  )
+  .forEach((file) => {
+    modelDefiners.push(require(path.join(__dirname, "/models", file)));
+  });
 
 // Injectamos la conexion (sequelize) a todos los modelos
 modelDefiners.forEach((model) => model(sequelize));
 // Capitalizamos los nombres de los modelos ie: product => Product
 let entries = Object.entries(sequelize.models);
 let capsEntries = entries.map((entry) => [
-   entry[0][0].toUpperCase() + entry[0].slice(1),
-   entry[1],
+  entry[0][0].toUpperCase() + entry[0].slice(1),
+  entry[1],
 ]);
 sequelize.models = Object.fromEntries(capsEntries);
 
@@ -69,6 +74,6 @@ Country.belongsToMany(Activity, { through: "country_activity" });
 Activity.belongsToMany(Country, { through: "country_activity" });
 
 module.exports = {
-   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
-   conn: sequelize, // para importart la conexión { conn } = require('./db.js');
+  ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
+  conn: sequelize, // para importart la conexión { conn } = require('./db.js');
 };
